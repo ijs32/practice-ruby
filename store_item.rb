4627777a80d4f703f089cde1e548099de0075875
Ian S <iansilber32@gmail.com>
2021-10-06 13:40:29 -0400

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,60 @@
+# Use the terminal to create a new folder in your actualize folder called practice-ruby.
+# Create a new file in the folder called store_item.rb and open it in your text editor.
+# Use hashes with symbols to represent the following scenario:
+# You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+# Represent 3 items using hashes. Each hash should have the same keys with different values.
+# Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+
+#represent store items using array of hashes
+
+# store = [
+#   { item: "charger", color: "white", price: "$29.99" },
+#   { item: "phone", color: "space grey", price: "$699.99" },
+#   { :item => "watch", :color => "blue", :price => "$12,995" },
+# ]
+
+# p store[0]
+# puts "======================================"
+# p store[1]
+# puts "======================================"
+# p store[2]
+# puts "======================================"
+
+#represent store items using a class
+
+class Store_item
+  def initialize(item_name, color, price)
+    @item_name = item_name
+    @color = color
+    @price = price
+  end
+
+  def item=(item)
+    @item_name = item
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price_inflation
+    @price *= 1.05
+  end
+
+  def print_info
+    puts "#{@item_name}, #{@color}, #{@price}"
+    return
+  end
+end
+
+item1 = Store_item.new("charger", "white", 29.99)
+item2 = Store_item.new("phone", "space grey", 699.99)
+
+item1.color = "green"
+item1.price_inflation
+item1.print_info
+item2.print_info
