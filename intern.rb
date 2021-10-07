9bfaee4ed3111db74e223faafb948d875bf6550a
Ian S <iansilber32@gmail.com>
2021-10-07 12:58:17 -0400

    adds example of module in intern.rb file

--- intern.rb
+++ intern.rb
@@ -1,3 +1,11 @@
+module EmailReportable
+  def send_report
+    puts "Sending email..."
+    # use email sending library...
+    puts "Email sent!"
+  end
+end
+
 class Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active, :salary
@@ -19,6 +27,8 @@ class Employee
 end
 
 class Manager < Employee
+  include EmailReportable
+
   def initialize(input_options)
     super
     @employees = input_options[:employees]
@@ -37,24 +47,15 @@ class Manager < Employee
       end
     end
   end
-
-  def send_report
-    puts "Sending email..."
-    # use email sending library...
-    puts "Email sent!"
-  end
 end
 
 employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
 employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 
-manager.print_info
-manager.send_report
-manager.give_all_raises
-manager.fire_all
+class Intern < Employee
+  include EmailReportable
+end
 
-employee1.print_info
-employee2.print_info
-p employee1.active
-p employee2.active
+intern = Intern.new(first_name: "Ian", last_name: "Silber", salary: 1000000000, active: true)
+intern.send_report
