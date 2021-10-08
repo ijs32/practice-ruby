6bb0ee3ad213d533d8ffc037e58d1cd774d6547d
IanJSilber <iansilber32@gmail.com>
2021-10-08 11:45:54 -0400

    adds second premium item from on my linux desktop as a test run commit

--- store-exercise/main.rb
+++ store-exercise/main.rb
@@ -3,10 +3,10 @@ require_relative "premium_item"
 
 item1 = Store_item.new({ item: "charger", color: "white", price: "$29.99" })
 item2 = Store_item.new({ item: "phone", color: "space grey", price: "$699.99" })
-premium_item = Premium_items.new(item: "Golden Lambo", color: "Gold", price: "$100,000,000.99", availability: "Unavailable")
-premium_item.scream
-p premium_item.availability
-p premium_item.color
-premium_item.print_info
+premium_item1 = Premium_items.new(item: "Golden Lambo", color: "Gold", price: "$100,000,000.99", availability: "Unavailable")
+premium_item2 = Premium_items.new(item: "Rolex", color: "navy", price: "$45,000", availability: "available")
+premium_item1.scream
+premium_item2.print_info
+premium_item1.print_info
 item1.print_info
 item2.print_info
