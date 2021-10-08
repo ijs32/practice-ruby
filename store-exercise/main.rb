b31918d6a9822afd7cb250afeae3f9fe03029155
IanJSilber <iansilber32@gmail.com>
2021-10-08 11:54:51 -0400

    tests new branch of practice ruby folder

--- store-exercise/main.rb
+++ store-exercise/main.rb
@@ -5,8 +5,10 @@ item1 = Store_item.new({ item: "charger", color: "white", price: "$29.99" })
 item2 = Store_item.new({ item: "phone", color: "space grey", price: "$699.99" })
 premium_item1 = Premium_items.new(item: "Golden Lambo", color: "Gold", price: "$100,000,000.99", availability: "Unavailable")
 premium_item2 = Premium_items.new(item: "Rolex", color: "navy", price: "$45,000", availability: "available")
+premium_item3 = Premium_items.new(item: "Linux Desktop", color: "black", price: "$600.00", availability: "now working")
 premium_item1.scream
-premium_item2.print_info
 premium_item1.print_info
+premium_item2.print_info
+premium_item3.print_info
 item1.print_info
 item2.print_info
